cc372c8e2773b10d049586da8a5b58f27b379be9
praiseadegokee <praiseadegokee@gmail.com>
2022-12-05 14:13:43 -0800

    Task 11. Delete at

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-# Check if the index is out of range
-    if idx < 0 or idx >= len(my_list):
-        return my_list
-# Create a new list with the same elements as the original list,
-# except for the element at the specified index
-    new_list = []
-    for i, item in enumerate(my_list):
-        if i != idx:
-            new_list.append(item)
-    
-    return new_list
+    if(idx < 0 or idx >= len(my_list)):
+        copyList = my_list.copy()
+        return(copyList)
+    del my_list[idx]
+    copyList = my_list.copy()
+    return(copyList)
